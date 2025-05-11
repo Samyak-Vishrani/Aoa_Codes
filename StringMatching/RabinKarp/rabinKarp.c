40041f5b46f13a1f46224758e24e44d32fa65274
Samyak-Vishrani <samyak.vishrani@gmail.com>
2025-05-11 23:14:22 +0530

    rabinKarp

---
+++ StringMatching/RabinKarp/rabinKarp.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void rabinKarp(char pat[], char txt[], int n, int m) {
+
+    int code = 0;
+    int i = 0;
+    while(pat[i] != '\0') {
+        int digit = pat[i] - 'A' + 1;
+        code = code * 10 + digit;
+        i++;
+    }
+    
+    // j from 0 to  = n-m
+    for(int j = 0; j <= n-m; j++) {
+        int txtCode = 0;
+        int i = 0;
+        while(i != m) {
+            int digit = txt[j + i] - 'A' + 1;
+            txtCode = txtCode * 10 + digit;
+            i++;
+        }
+
+        if(txtCode == code) {
+            printf("\nPattern found at %d\n", j);
+            return;
+        }
+    }
+
+}
+
+int main() {
+
+    int n = 15;
+    int m = 5;
+    char txt[] = "ABABCABCABABABD";
+    char pat[] = "ABABD";
+
+    rabinKarp(pat, txt, n, m);
+
+    return 0;
+}
