f06f0e215958c925bf84787e7e74621a4886d3fb
Samyak-Vishrani <samyak.vishrani@gmail.com>
2025-05-14 23:12:17 +0530

    insertionSort

---
+++ Sorting/insertionSort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+
+    int key;
+
+    for(int i = 1; i < n; i++) {
+        key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    // print
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+}
+
+int main() {
+    int n = 5;
+    int arr[] = {5, 1, 9, 3, -7};
+    insertionSort(arr, n);
+}
