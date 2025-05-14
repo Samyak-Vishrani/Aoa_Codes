f0409a5d0022f72887c344540e711a9b95c6fc4f
Samyak-Vishrani <samyak.vishrani@gmail.com>
2025-05-14 21:24:40 +0530

    matrixMultiplication

---
+++ Matrix_Chain_Multiplication/matrixChainMultiplication.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define n 6
+
+int m[n][n];
+// int k[n][n];
+
+void matrixChainMulti(int p[]) {
+    int i, j, k;
+
+    // initialize diagonals to 0
+    for(i = 0; i < n; i++){
+        m[i][i] = 0;
+    }
+    
+    // loops are like this as we have to fill 'm' diagonally
+    for(int l = 2; l < n; l++) {
+        for(i = 1; i < n - l + 1; i++) {
+            j = i + l - 1;
+            int min = INT_MAX;
+            for(k = i; k < j; k++){
+                int q = (m[i][k] + m[k + 1][j] + (p[i - 1] * p[j] * p[k]));
+                if(q < min)
+                    min = q;
+            }
+            m[i][j] = min;
+        }
+    }
+
+    for(i = 1; i < n; i++) {
+        for(j = 1; j < n; j++) {
+            if (i > j)
+                printf("-\t");
+            else
+                printf("%d\t", m[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nMinimum Multiplication needed: %d", m[1][n - 1]);
+
+}
+
+
+int main() {
+
+    int p[] = {4, 10, 3, 12, 20, 7};
+    matrixChainMulti(p);
+
+}
